6b07e2f160dbfdef67396326229ed7e60931634f
Alberto G. Valerio <albertogvalerio@Albertos-MBP-2.homenet.telecomitalia.it>
2024-03-08 22:41:35 +0100

    Data exploration with 2D plots

---
+++ notebooks/utils/plotting.py
@@ -0,0 +1,96 @@
+"""
+A set of plotting functions
+"""
+import matplotlib.pyplot as plt
+import random
+import os
+from nilearn import plotting
+import nibabel as nib
+from utils.utils import get_colored_mask, get_slice
+
+
+def plot_random_samples(folder, n_samples, axis):
+	"""Plot sample data.
+	Args:
+		folder (str): the path of the folder containing data.
+		n_samples (int): number of samples to plot. Min value is 2.
+		axis (int): axis of the spatial image. Values are: 0=X_axis, 1=Y_axis, 2=Z_axis.
+	Returns:
+		None.
+	"""
+	if n_samples > 1 and axis < 3:
+		samples = random.sample(os.listdir(folder), n_samples)
+		fig, axs = plt.subplots(n_samples, 5, figsize=(18, n_samples * 4))
+		for i, axl in enumerate(axs):
+			images = sorted(os.listdir(os.path.join(folder, samples[i])))
+			images.append(images.pop(images.index(images[0])))
+			if i == 0:
+				axl[0].set_title('post-contrast T1-weighted')
+				axl[1].set_title('T1-native')
+				axl[2].set_title('T2-FLAIR')
+				axl[3].set_title('T2-weighted')
+				axl[4].set_title('Segmented')
+			for j, ax in enumerate(axl):
+				brain_vol = nib.load(os.path.join(os.path.join(folder, samples[i]), images[j]))
+				isocenter = list(map(int, plotting.find_xyz_cut_coords(brain_vol)))
+				if j == len(images) - 1:
+					mask_colored = get_colored_mask(get_slice(brain_vol, axis, isocenter[axis]))
+					brain_vol = nib.load(os.path.join(os.path.join(folder, samples[i]), images[j - 1]))
+					ax.imshow(get_slice(brain_vol, axis, isocenter[axis]), cmap='gist_yarg')
+					ax.imshow(mask_colored)
+				else:
+					ax.imshow(get_slice(brain_vol, axis, isocenter[axis]), cmap='gist_yarg')
+				ax.spines['top'].set_visible(False)
+				ax.spines['right'].set_visible(False)
+				ax.spines['bottom'].set_visible(False)
+				ax.spines['left'].set_visible(False)
+				a = {0:'X',1:'Y',2:'Z'}
+				ax.set_xlabel(a[axis]+'='+str(isocenter[axis]))
+				ax.get_xaxis().set_ticks([])
+				ax.get_yaxis().set_ticks([])
+		fig.tight_layout()
+		plt.show()
+	else:
+		print('\n' + ''.join(['> ' for i in range(40)]))
+		print('\nERROR: \033[95m n_samples\033[0m must be greater that \033[95m 1\033[0m and \033[95m axis\033[0m bust be lower that \033[95m 2\033[0m.\n')
+		print(''.join(['> ' for i in range(40)]) + '\n')
+
+
+def plot_single_sample(folder):
+	"""Plot sample data.
+	Args:
+		folder (str): the path of the folder containing data.
+	Returns:
+		None.
+	"""
+	samples = random.sample(os.listdir(folder), 1)
+	images = sorted(os.listdir(os.path.join(folder, samples[0])))
+	images.append(images.pop(images.index(images[0])))
+	fig, axs = plt.subplots(3, 5, figsize=(18, 12))
+	for i, axl in enumerate(axs):
+		if i == 0:
+			axl[0].set_title('post-contrast T1-weighted')
+			axl[1].set_title('T1-native')
+			axl[2].set_title('T2-FLAIR')
+			axl[3].set_title('T2-weighted')
+			axl[4].set_title('Segmented')
+		for j, ax in enumerate(axl):
+			brain_vol = nib.load(os.path.join(os.path.join(folder, samples[0]), images[j]))
+			isocenter = list(map(int, plotting.find_xyz_cut_coords(brain_vol)))
+			if j == len(images) - 1:
+				mask_colored = get_colored_mask(get_slice(brain_vol, i, isocenter[i]))
+				brain_vol = nib.load(os.path.join(os.path.join(folder, samples[0]), images[j - 1]))
+				ax.imshow(get_slice(brain_vol, i, isocenter[i]), cmap='gist_yarg')
+				ax.imshow(mask_colored)
+			else:
+				ax.imshow(get_slice(brain_vol, i, isocenter[i]), cmap='gist_yarg')
+			ax.spines['top'].set_visible(False)
+			ax.spines['right'].set_visible(False)
+			ax.spines['bottom'].set_visible(False)
+			ax.spines['left'].set_visible(False)
+			a = {0:'X',1:'Y',2:'Z'}
+			ax.set_xlabel(a[i]+'='+str(isocenter[i]))
+			ax.get_xaxis().set_ticks([])
+			ax.get_yaxis().set_ticks([])
+	fig.tight_layout()
+	plt.show()
--- notebooks/utils/utils.py
+++ notebooks/utils/utils.py
@@ -8,6 +8,7 @@ import zipfile
 from utils.config import get_config
 import synapseclient
 import synapseutils
+import numpy as np
 
 
 def make_dataset(dataset):
@@ -17,8 +18,7 @@ def make_dataset(dataset):
 	Args:
 		dataset (str): the dataset name (See SYN_IDS keys in config.py).
 	Returns:
-		train_path (str): the full path of the training set.
-		eval_path (str): the full path of the evaluation set.
+		paths (list): the full path of the training and evaluation sets.
 	"""
 	_base_path = '\\'.join(os.getcwd().split('\\')[:-1]) + '\\' if platform == 'win32' else '/'.join(os.getcwd().split('/')[:-1]) + '/'
 	_config = get_config()
@@ -28,29 +28,90 @@ def make_dataset(dataset):
 	if platform == 'win32':
 		_data_path = _data_path.replace('/', '\\')
 	if _env.get('AUTH_TOKEN') is not None:
-		syn = synapseclient.Synapse()
-		syn.login(authToken=_env.get('AUTH_TOKEN'))
-		print('Downloading files...')
-		files = synapseutils.syncFromSynapse(syn, _syn_id, path=_data_path)
-		paths = [
-			os.path.join(_data_path, [f.name for f in files if 'training' in f.name.lower()][0]),
-			os.path.join(_data_path, [f.name for f in files if 'validation' in f.name.lower()][0])
-		]
-		try:
-			print('Extracting files...')
-			for p in paths:
-				with zipfile.ZipFile(p, 'r') as zip_ref:
-					zip_ref.extractall(_data_path)
-			print('Finalizing...')
-			for i in os.listdir(_data_path):
-				full = os.path.join(_data_path, i)
-				if os.path.isfile(full):
-					os.unlink(full)
-			print('Operation completed!')
-			return paths[0], paths[1]
-		except OSError as e:
-			print(e)
+		if not os.path.isdir(_data_path):
+			os.makedirs(_data_path)
+		if len(os.listdir(_data_path)) == 0:
+			syn = synapseclient.Synapse()
+			syn.login(authToken=_env.get('AUTH_TOKEN'))
+			print('Downloading files...')
+			files = synapseutils.syncFromSynapse(syn, _syn_id, path=_data_path)
+			paths = [
+				os.path.join(_data_path, [f.name for f in files if 'training' in f.name.lower()][0]),
+				os.path.join(_data_path, [f.name for f in files if 'validation' in f.name.lower()][0])
+			]
+			try:
+				print('Extracting files...')
+				for p in paths:
+					with zipfile.ZipFile(p, 'r') as zip_ref:
+						zip_ref.extractall(_data_path)
+				print('Finalizing...')
+				for i in os.listdir(_data_path):
+					full = os.path.join(_data_path, i)
+					if os.path.isfile(full):
+						os.unlink(full)
+				print('Operation completed!')
+				return [paths[0][:-4], paths[1][:-4]]
+			except OSError as e:
+				print(e)
+		else:
+			paths = [f for f in os.listdir(_data_path) if os.path.isdir(os.path.join(_data_path, f))]
+			paths = [
+				os.path.join(_data_path, [f for f in paths if 'training' in f.lower()][0]),
+				os.path.join(_data_path, [f for f in paths if 'validation' in f.lower()][0])
+			]
+			print('\n' + ''.join(['> ' for i in range(30)]))
+			print('\nWARNING: \033[95m '+dataset+'\033[0m directory not empty.\n')
+			print('TRAIN_PATH: \033[95m '+'/'.join(paths[0].split('/')[-2:])+'\033[0m')
+			print('EVAL_PATH: \033[95m '+'/'.join(paths[1].split('/')[-2:])+'\033[0m \n')
+			print(''.join(['> ' for i in range(30)]) + '\n')
+			return paths if len(paths) == 2 else ['', '']
 	else:
 		print('\n' + ''.join(['> ' for i in range(30)]))
 		print('\nERROR: missing auth token! Please check your\033[95m .env\033[0m file.\n')
 		print(''.join(['> ' for i in range(30)]) + '\n')
+		return ['','']
+
+
+def get_colored_mask(mask):
+	"""Convert 2D segmentation mask into RGBA image.
+	Args:
+		mask (numpy.ndarray): the 2D mask.
+	Returns:
+		mask_colored (numpy.ndarray): the RGBA colored mask.
+	"""
+	mask_colored = np.zeros((mask.shape[0], mask.shape[1], 4), dtype='uint8')
+	for i in range(mask.shape[0]):
+		for j in range(mask.shape[1]):
+			match mask[i][j]:
+				case 0.:
+					mask_colored[i][j] = [255, 255, 255, 0]
+				case 1.:
+					mask_colored[i][j] = [255, 0, 0, 255]
+				case 2.:
+					mask_colored[i][j] = [255, 255, 0, 255]
+				case 3.:
+					mask_colored[i][j] = [0, 255, 0, 255]
+	return mask_colored
+
+
+def get_slice(spatial_image, axis, offset):
+	"""Get a 2D slice of a spatial image.
+	Args:
+		spatial_image (nibabel.nifti1.Nifti1Image): the spatial image.
+		axis (int): axis of the spatial image. Values are: 0=X_axis, 1=Y_axis, 2=Z_axis.
+		offset (int): offset value where to cut the slice.
+	Returns:
+		slice (numpy.ndarray): the 2D slice.
+	"""
+	spatial_image_data = spatial_image.get_fdata()
+	slice = np.array([])
+	match axis:
+		case 0:
+			slice = np.rot90(spatial_image_data[offset,:,:], 1)
+		case 1:
+			slice = np.rot90(spatial_image_data[:,offset,:], 1)
+		case 2:
+			slice = np.rot90(spatial_image_data[:,:,offset], 3)
+		case _:
+			raise Exception('Axis not valid. Possible values are: 0, 1, 2')
+	return slice
