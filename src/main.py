6952405a1f5182b204437980173267953c26e4b3
Alberto G. Valerio <albertogvalerio@gmail.com>
2024-03-21 12:25:16 +0100

    Minor fixes

--- src/main.py
+++ src/main.py
@@ -107,6 +107,7 @@ eval_transform = Compose(
             pixdim=(1.0, 1.0, 1.0),
             mode=('bilinear', 'nearest'),
         ),
+        RandSpatialCropd(keys=['image', 'label'], roi_size=[128, 128, 128], random_size=False),
         NormalizeIntensityd(keys='image', nonzero=True, channel_wise=True),
     ]
 )
